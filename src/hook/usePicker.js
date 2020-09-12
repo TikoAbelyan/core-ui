28c50d901f8cf783713bc25bb18ea9b2751336a9
Tiko_Abelyan <tikoabelyan1995@gmail.com>
2020-09-12 22:18:16 +0400

    separate Hooks, fixed cors-origin, change fetch to axios

--- src/hook/usePicker.js
+++ src/hook/usePicker.js
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from "moment";
+import axios from "axios";
 
 export const usePicker = () => {
   const [date, setDate] = React.useState({ startDate: null, endDate: null });
@@ -23,54 +24,25 @@ export const usePicker = () => {
       deviceType: "WELL",
       measurementType: "NORMAL",
     };
-    //https://cors-anywhere.herokuapp.com/
-    //for the cors origin
-    //put https://cors-anywhere.herokuapp.com/http://mesurit.com:8086/api/data/search/
-    try {
-      const response = await fetch("http://mesurit.com:8086/api/data/search/", {
-        method: "POST",
-        mode: "cors",
-        credentials: "same-origin",
-        headers: {
-          Authorization: `Basic ${btoa(`Guest:WaterFlow`)}`,
-          "Content-Type": "application/json",
+
+    axios
+      .post(
+        "api/data/search/",
+        {
+          ...requestBody,
         },
-        body: JSON.stringify(requestBody),
+        {
+          headers: {
+            Authorization: `Basic ${btoa(`Guest:WaterFlow`)}`,
+          },
+        }
+      )
+      .then((response) => {
+        setChartData(response.data[0]);
+      })
+      .catch((error) => {
+        console.trace(error);
       });
-      const [result] = await response.json();
-      //if you want to show result please uncomment  setChartData(result); and comment down hardcode
-      // setChartData(result);
-      console.log("result", result);
-    } catch (error) {
-      console.trace(error);
-    }
-    //HARDCODE
-    setChartData({
-      chartConfig: {
-        input: 1,
-        chartType: "column",
-        textToShow: "Well",
-        inputType: "Well",
-        dateFormat: "%s/%s/%s %s:%s",
-      },
-      data: [
-        ["01/08/20 00:00", 595],
-        ["02/08/20 00:00", 975],
-        ["03/08/20 00:00", 1032],
-        ["04/08/20 00:00", 1708],
-        ["05/08/20 00:00", 1875],
-        ["06/08/20 00:00", 338],
-        ["07/08/20 00:00", 799],
-        ["08/08/20 00:00", 1293],
-        ["09/08/20 00:00", 899],
-        ["10/08/20 00:00", 845],
-        ["11/08/20 00:00", 15],
-        ["12/08/20 00:00", 1579],
-        ["13/08/20 00:00", 1095],
-        ["14/08/20 00:00", 1221],
-        ["15/08/20 00:00", 775],
-      ],
-    });
   };
 
   function submitFilter({ startDate, endDate }) {
